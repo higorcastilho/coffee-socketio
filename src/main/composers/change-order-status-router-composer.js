63e485a0a6586cac069b450f93829b08a6ed3905
Higor Castilho <castilhohf@gmail.com>
2021-01-04 07:28:36 -0300

    refactor: remove unused imports from change-order-status-router-composer

--- src/main/composers/change-order-status-router-composer.js
+++ src/main/composers/change-order-status-router-composer.js
@@ -1,11 +1,7 @@
 const ChangeOrderStatusRouter = require('../../presentation/routes/change-order-status-router')
-const UpdateOrderStatusUseCase = require('../../domain/usecases/update-order-status-usecase')
-const EmitLiveData = require('../../utils/helpers/emit-live-data')
 
 module.exports = class SocketioRouterComposer {
 	static compose () {
-		const emitLiveData = new EmitLiveData()
-		const updateOrderStatusUseCase = new UpdateOrderStatusUseCase(emitLiveData)
-		return new ChangeOrderStatusRouter(updateOrderStatusUseCase)
+		return new ChangeOrderStatusRouter()
 	}
 }
